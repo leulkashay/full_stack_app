8a2e3199b58f870477f8312fef2304c57d6760a1
leulkashay <leulkashay2012@gmail.com>
2023-11-21 14:08:50 +0300

    handle error

--- node_shop/server.js
+++ node_shop/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 const orderRoutes = require('./routes/order');
 const productRoutes = require('./routes/product');
@@ -14,16 +15,16 @@ const port = process.env.PORT || 8082;
 //middleware
 app.use(bodyParser.json({limit: "30mb", exyended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
-
+app.use(morgan('dev'));
 mongoose.connect(process.env.MONGO_URI, {
    useNewUrlParser: true,
-    useUnifiedTopology: false
+    useUnifiedTopology: false,
 });
 
 // db connected
-mongoose.connection.once('open', () => app.listen(port, () => {
-    console.log(`server is running on port ${port}`);
-}));
+mongoose.connection.once('open', () => {
+    console.log("connected DB");
+});
 
 
 
@@ -35,3 +36,22 @@ app.use('/', (req, res, next) => {
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+app.use((req,res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status( error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
+
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+});
